4845dbf8e4fefe840e3deb41c688b4cfd4bdbfc7
AlexSaeChao <chaoalex93@gmail.com>
2023-07-27 19:45:51 -0700

    added spring stuff

---
+++ src/test/java/com/operator/bcrypt/BCryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operator.bcrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BCryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
